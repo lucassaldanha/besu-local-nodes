2e6b567c0e304c2c3fdae87a0d994c8e06eaa927
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-03-23 11:38:20 +1000

    added tessera configs and updated cleanup script
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- delete_databases.sh
+++ delete_databases.sh
@@ -5,6 +5,10 @@ rm -rf workdir/orion1/routerdb workdir/orion1/orion.ports
 rm -rf workdir/orion2/routerdb workdir/orion2/orion.ports
 rm -rf workdir/orion3/routerdb workdir/orion3/orion.ports
 
+rm -rf workdir/orion1/tessera1*
+rm -rf workdir/orion2/tessera2*
+rm -rf workdir/orion3/tessera3*
+
 rm -rf workdir/besu1/data
 rm -rf workdir/besu2/data
 rm -rf workdir/besu3/data
