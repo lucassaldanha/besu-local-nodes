9e36e08569843b00886324c9b37a91f1df2fd8ec
Lucas Saldanha <lucascrsaldanha@gmail.com>
2021-11-09 14:33:34 +1300

    Updated ethsigner run script

--- scripts/run-ethsigner.sh
+++ scripts/run-ethsigner.sh
@@ -2,4 +2,12 @@
 
 echo "Starting EthSigner"
 
-ethsigner --chain-id=2018 --http-listen-port=9545 --downstream-http-port=8545 file-based-signer --key-file=config/ethsigner/keyFile --password-file=config/ethsigner/passwordFile
+ethsigner \
+--chain-id=2018 \
+--http-cors-origins=all \
+--http-listen-host=0.0.0.0 \
+--http-listen-port=9545 \
+--downstream-http-port=8545 \
+file-based-signer \
+--key-file=config/ethsigner/keyFile \
+--password-file=config/ethsigner/passwordFile
